7cbe223eebcd4a27480b00983bbb9dcc3b2a2607
AndrewDl <dlandrew@ukr.net>
2019-04-07 21:28:53 +0200

    Initial commit

---
+++ PartyDemo/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PartyDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.CurrentTime = DateTime.Now;
+            return View("Index");
+        }
+    }
+}
